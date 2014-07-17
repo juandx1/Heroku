3b274136c617d4f30aefd0e0cd364c8209362404
juandx1 <juandx1@hotmail.com>
2014-07-17 17:38:07 -0500

    Commit Desde Mendez

--- Ministro/models.py
+++ Ministro/models.py
@@ -1,4 +1,17 @@
 from django.db import models
-from App import models
 
 # Create your models here.
+
+class Usuario_Ministro(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(max_length=140,blank=True, null=True)
+    telefono = models.IntegerField(blank=True,null=True, max_length=15)
+    direccion = models.CharField(max_length=200,blank=True, null=True)
+    barrio = models.CharField(max_length=100,blank=True, null=True)
+    profesion = models.CharField(max_length=200, blank=True, null=True)
+    cedula = models.CharField(max_length=150, blank=True, null=True)
+    ocupacion = models.CharField(max_length=200, blank=True, null=True)
+    bautizado = models.BooleanField(default=False)
+    ciudad = models.CharField(max_length=200, blank=True, null=True)
+    pais = models.CharField(max_length=200, blank=True, null=True)
