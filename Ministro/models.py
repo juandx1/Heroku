ef13abfe33d7fd05ae2a62e5cbd274d13ab561e9
overlord0915 <onatra0915@gmail.com>
2014-07-17 17:38:47 -0500

    Commit desde onatra

--- Ministro/models.py
+++ Ministro/models.py
@@ -1,4 +1,11 @@
 from django.db import models
-from App import models
+from App.models import *
 
 # Create your models here.
+class Cuenta(models.Model):
+    TIPO = (
+        ('A', 'Activo'),
+        ('P', 'Pasivo'),
+        ('C', 'Capital'),
+    )
+    tipo=models.CharField(max_length=1, choices=TIPO)   
