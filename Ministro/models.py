2b7dcdf2e4690e22e923a197b858a576e4c8425f
overlord0915 <onatra0915@gmail.com>
2014-07-17 18:25:56 -0500

    Commit modelo contabilidad

--- Ministro/models.py
+++ Ministro/models.py
@@ -11,9 +11,23 @@ class Cuenta(models.Model):
     tipo=models.CharField(max_length=1, choices=TIPO)
 
 
-class grupo(models.Model):
+class Grupo(models.Model):
     nombre = models.CharField(max_length=100)
     cuenta=models.ForeignKey(Cuenta)
+
+
+class Concepto(models.Model):
+    numero = models.IntegerField()
+    nombre = models.CharField(max_length=100)
+    grupo = models.ForeignKey(Grupo)
+
+class Registro(models):
+    numero=models.IntegerField()
+    fecha=models.DateField()
+    comentario=models.CharField(max_length=140)
+    desposito=models.IntegerField()
+    retiro=models.IntegerField()
+
 
 # Create your models here.
 
@@ -31,17 +45,3 @@ class Usuario_Ministro(models.Model):
     ciudad = models.CharField(max_length=200, blank=True, null=True)
     pais = models.CharField(max_length=200, blank=True, null=True)
 
-class Campo_Personalizado(models.Model):
-    nombre = models.CharField(max_length=100)
-    iglesia = models.ForeignKey(Iglesia)
-
-class Contenido_Campo_Personalizado(models.Model):
-    contenido = models.CharField(max_length=200)
-    campo = models.ForeignKey(Campo_Personalizado)
-    usuario = models.ForeignKey(Usuario_Ministro)
-
-class Rango(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=500)
-    iglesia = models.ForeignKey(Iglesia)
-    usuario = models.ForeignKey(Usuario_Ministro)
